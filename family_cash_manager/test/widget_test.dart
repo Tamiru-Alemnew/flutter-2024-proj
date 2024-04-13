430e6509932115a4b8027d3e64c6a439e42cc52b
obsa <obsukebede2022@gmail.com>
2024-04-13 12:00:00 +0300

    flutter

--- family_cash_manager/test/widget_test.dart
+++ family_cash_manager/test/widget_test.dart
@@ -2,6 +2,8 @@
 
 // To perform an interaction with a widget in your test, use the WidgetTester
 
+
+
 // utility in the flutter_test package. For example, you can send tap and scroll
 
 // gestures. You can also use WidgetTester to find child widgets in the widget
