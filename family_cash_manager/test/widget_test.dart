5828540cdf38631a07cb3e25b17a895e30604010
Obsu Kebede <134233644+ob971@users.noreply.github.com>
2024-05-31 16:52:06 +0300

    Update widget_test.dart

--- family_cash_manager/test/widget_test.dart
+++ family_cash_manager/test/widget_test.dart
@@ -13,6 +13,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:family_cash_manager/main.dart';
 
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
